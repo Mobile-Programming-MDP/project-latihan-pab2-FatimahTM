cafd317cd644c74620b492b715573cb0d155dcec
Fatimah <tuzzahrafatimah505@gmail.com>
2025-04-23 19:58:35 +0700

    yes

--- fasum/lib/screens/home_screen.dart
+++ fasum/lib/screens/home_screen.dart
@@ -1,9 +1,24 @@
 import 'package:fasum/screens/sign_in_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; // Menggunakan DateFormat dari intl
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
+  String formatTime(DateTime dateTime) {
+    final now = DateTime.now();
+    final diff = now.difference(dateTime);
+    if (diff.inSeconds < 60) {
+      return '${diff.inSeconds} secs ago';
+    } else if (diff.inMinutes < 60) {
+      return '${diff.inMinutes} mins ago';
+    } else if (diff.inHours < 24) {
+      return '${diff.inHours} hrs ago';
+    } else {
+      return DateFormat('dd/MM/yyyy').format(dateTime);
+    }
+  }
 
   Future<void> signOut(BuildContext context) async {
     await FirebaseAuth.instance.signOut();
@@ -23,11 +38,31 @@ class HomeScreen extends StatelessWidget {
             onPressed: () {
               signOut(context);
             },
-            icon: Icon(Icons.logout),
+            icon: const Icon(Icons.logout),
           ),
         ],
       ),
-      body: const Center(child: Text("You have logged in")),
+      body: StreamBuilder(
+        // StreamBuilder, kamu bisa sesuaikan stream yang diinginkan
+        stream: FirebaseAuth.instance.authStateChanges(), // Example stream
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            return Center(child: Text('User is signed in: ${snapshot.data}'));
+          } else {
+            return const Center(child: Text('No user is signed in.'));
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Tambahkan aksi untuk FAB
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
